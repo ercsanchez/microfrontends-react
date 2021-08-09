42285707298dbd1194a6a498622e8fceffa9d32e
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-09 19:10:52 +0800

    5-42 generic integration of marketing and container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-
-import { mount } from "marketing/MarketingApp";
-
-console.log(mount);
+import MarketingAppComp from "./components/MarketingAppComp";
 
 export default () => {
-  return <h1>Container!</h1>;
+  return (
+    <div>
+      <h1>Container!</h1>
+      <hr />
+      <MarketingAppComp />
+    </div>
+  );
 };
