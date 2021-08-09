ea2422dbfcfce4647cccc240ba52823bbccbe4bb
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-09 10:28:45 +0800

    3-25 refactor cart for conditional rendering

--- ecomm/cart/src/bootstrap.js
+++ ecomm/cart/src/bootstrap.js
@@ -1,5 +1,21 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+// Context/Situation #1: isolated dev in Cart app
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart-in-isolation");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+// Context/Situation #2: render in Container app
+
+export { mount };
