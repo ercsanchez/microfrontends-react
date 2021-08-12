3d107ea78b202672e8acfa4b8fb4f51f5fcc638d
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-12 18:41:54 +0800

    8-62b test if automated cache invalidation is working by changing any file inside of container
    - change in App.js will cause rebuild of container's main.js
    - verify if CloudFront serves latest index.html of container by verifying the script tag if referencing latest built main.js file in S3 bucket
    - also verify if new invalidation was created for CloudFront distro

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingAppComp from "./components/MarketingAppComp";
 export default () => {
   return (
     <div>
-      <h1>Container!</h1>
+      <h1>Container!!!!</h1>
       <hr />
       <MarketingAppComp />
     </div>
