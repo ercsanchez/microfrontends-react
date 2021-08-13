cc824eed0acf785c807d1e492234384c4a8d9723
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-13 17:51:51 +0800

    9-75 fix material-ui's class name collisions | prepend namespaces to style class names during production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingAppComp from "./components/MarketingAppComp";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "cont",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingAppComp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingAppComp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
