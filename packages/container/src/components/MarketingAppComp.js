5808e47e1bf92783d8929e2ee9c735037089a6f9
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-15 04:35:04 +0800

    10-85b sync container's history object (browser hist) when marketing's history (memory hist) changes | upwards communication from marketing to container

--- packages/container/src/components/MarketingAppComp.js
+++ packages/container/src/components/MarketingAppComp.js
@@ -1,13 +1,28 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
 
+  const history = useHistory();
+
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: (location) => {
-        console.log(location); // location arg passed by history.listen
+      onNavigate: ({ pathname: nextPathname }) => {
+        console.log(nextPathname); // pathname property of location obj
+
+        // prevent infinite loop caused by updating of histories in container and marketing
+
+        // figure out if current path is diff. from next path
+        const { pathname } = history.location;
+
+        // if same path, do not alter browser history
+
+        // if diff paths, update browser history
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
       },
     });
   });
