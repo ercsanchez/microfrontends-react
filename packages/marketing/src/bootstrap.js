94996a177aac7158d4fd494da720622e0a53ca31
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-14 22:22:02 +0800

    10-81 using memory history in marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 
 import App from "./App";
 
 // Mount function to start up app
 
 const mount = (el) => {
-  ReactDOM.render(<App />, el);
+  const history = createMemoryHistory();
+
+  ReactDOM.render(<App history={history} />, el);
 };
 
 // If we are in dev and in isolation,
