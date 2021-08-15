0e8f4dd0921d06f9c7df785a6e8b7bdbe5508811
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-15 16:26:22 +0800

    11-89 starting setup for auth mfe/app

---
+++ packages/auth/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js", // to resolve caching issues
+    publicPath: "/auth/latest/", // Webpack plugins will (HtmlWebpackPlugin) reference files built by webpack using this path so that remoteEntry.js references correct file path
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "auth",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./AuthApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+
+import App from "./App";
+
+// Mount function to start up app
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // use browser history if provided, otherwise use memory history
+  const history = defaultHistory || createMemoryHistory();
+
+  // check if onNavigate was actually passed
+  // only happens when container is run and marketing is not running in isolation
+  if (onNavigate) {
+    history.listen(onNavigate); // memory history
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log(nextPathname);
+
+      // prevent infinite loop caused by updating of histories in marketing and container
+      const { pathname } = history.location;
+
+      // if same path, do not alter memory history
+
+      // if diff paths, update memory history
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in dev and in isolation,
+// call mount immediately
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+
+  if (devRoot) {
+    // provide browser history if in dev and running marketing in isolation
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+
+export { mount };
